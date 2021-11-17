ee4d352bd5b50e7d9cd23d092eec43f7cd59234d
Zach Leatherman <zachleatherman@gmail.com>
2021-11-17 17:48:54 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Squirminal
+
+A lightweight web component helper for HTML5 videos. Intended for use with `muted` by default HTML5 videos.
+
+* [Demo](https://zachleat.github.io/video-radio-star/demo.html)
+
+
+<!--
+## Installation
+
+```
+npm install @zachleat/squirminal
+```
+-->
+
+## Usage
+
+```html
+<link rel="stylesheet" href="squirminal.css">
+<squirm-inal>
+[2021-11-17T23:41:07.790Z]  "GET /favicon.ico" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+[2021-11-17T23:41:07.791Z]  "GET /favicon.ico" Error (404): "Not found"
+[2021-11-17T23:41:41.895Z]  "GET /demo.html" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0"
+</squirm-inal>
+<script type="module" src="squirminal.js"></script>
+```
+
+## Features
+
+* `<squirm-inal cursor>`
+* `<squirm-inal autoplay>`
+* Adds a button (for mobile)
+* TODO: keyboard activation
+
+## Credits
+
+* [MIT](./LICENSE)
---
+++ squirminal.js
@@ -0,0 +1,138 @@
+class Squirminal extends HTMLElement {
+  connectedCallback() {
+    // reduce motion
+    if(window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      return;
+    }
+
+    this.speed = 1.5; // higher is faster, 3 is about the fastest it can go.
+    this.chunkSize = {
+      min: 20,
+      max: 60
+    };;
+    this.attr = {
+      cursor: "cursor",
+      autoplay: "autoplay",
+    };
+    this.classes = {
+      showCursor: "squirminal-cursor-show",
+      content: "squirminal-content",
+    };
+    this.events = {
+      start: "squirminal.start",
+      end: "squirminal.end",
+    };
+
+    this.paused = true;
+    this.originalText = this.innerText;
+    this.queue = this.originalText.split("");
+
+    this.init();
+
+    if(this.hasAttribute(this.attr.autoplay)) {
+      this._whenVisible(this, (isVisible) => {
+        if(isVisible) {
+          this.play();
+        }
+      });
+    }
+  }
+
+  init() {
+    // add classes
+    this.classList.toggle(this.classes.showCursor, this.hasAttribute(this.attr.cursor));
+
+    // clear it out
+    this.innerHTML = "";
+
+    // Add button
+    let btn = document.createElement("button");
+    btn.innerText = "Play";
+    btn.addEventListener("click", e => {
+      if(!this.queue.length) {
+        this.reset();
+      } else {
+        this.toggle();
+      }
+    })
+    this.appendChild(btn);
+    this.playButton = btn;
+
+    // Add content div
+    let content = document.createElement("div");
+    content.classList.add(this.classes.content);
+    this.appendChild(content);
+    this.content = content;
+  }
+
+  reset() {
+    this.paused = true;
+    this.playButton.innerText = "Play";
+    this.content.innerHTML = "";
+    this.queue = this.originalText.split("");
+  }
+
+  _whenVisible(el, callback) {
+    if(!('IntersectionObserver' in window)) {
+      // run by default without intersectionobserver
+      callback(undefined);
+      return;
+    }
+  
+    return new IntersectionObserver(entries => {
+      entries.forEach(entry => callback(entry.isIntersecting))
+    }).observe(el);
+  }
+
+  toggle() {
+    if(this.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+
+  pause() {
+    this.paused = true;
+    this.playButton.innerText = "Play";
+  }
+
+  play() {
+    this.paused = false;
+    requestAnimationFrame(() => this.showMore());
+  }
+
+  showMore() {
+    if(this.paused) {
+      return;
+    }
+
+    if(!this.queue.length) {
+      this.playButton.innerText = "Reset";
+      return;
+    }
+
+    this.playButton.innerText = "Pause";
+
+    let add = [];
+    let chunkSize = Math.round(Math.max(this.chunkSize.min, Math.random() * this.chunkSize.max + 1));
+    for(let j = 0, k = chunkSize; j<k; j++) {
+      let character = this.queue.shift();
+      add.push(character);
+    }
+    
+    let strAdded = add.join("");
+    this.content.appendChild(document.createTextNode(strAdded));
+
+    // the amount we wait is based on how many non-whitespace characters printed to the screen in this chunk
+    let nonwhitespaceCharacters = strAdded.replace(/\s/gi, "");
+    let delay = nonwhitespaceCharacters.length * (1/this.speed);
+    setTimeout(() => {
+      requestAnimationFrame(() => this.showMore());
+    }, delay);
+  }
+}
+
+if("customElements" in window) {
+  window.customElements.define("squirm-inal", Squirminal);
+}
